087ec42cf88dabe188d706835d33608ee196bbe5
arlo-engineer <yoshikawa.engineer@gmail.com>
2025-01-19 17:40:23 +0900

    実装完了

--- resources/js/Pages/Tasks/Index.vue
+++ resources/js/Pages/Tasks/Index.vue
@@ -16,29 +16,63 @@ const props = defineProps<{
     tasks: Task[];
 }>();
 
-const filterCompletedStatus = ref(0);
-const filterCompletedTasks = computed(() => {
-    if (filterCompletedStatus.value === 0) {
-        return props.tasks;
-    } else if (filterCompletedStatus.value === 1) {
-        return props.tasks.filter((task) => task.completed === 0);
-    } else if (filterCompletedStatus.value === 2) {
-        return props.tasks.filter((task) => task.completed === 1);
-    }
-});
-
-function handleChangeFilter(event: Event) {
-    const target = event.target as HTMLSelectElement;
-    filterCompletedStatus.value = parseInt(target.value);
+enum FilterStatus {
+    All = 0,
+    Incomplete = 1,
+    Complete = 2,
 }
+
+enum SortStatus {
+    DueDate = "due_date",
+    CreatedAt = "created_at",
+}
+
+const filterCompletedStatus = ref<FilterStatus>(FilterStatus.All);
+const sortStatus = ref<SortStatus>(SortStatus.DueDate);
+
+// フィルター後のタスクリスト
+const filteredAndSortedTasks = computed(() => {
+    let tasks = props.tasks;
+    // フィルタリング
+    if (filterCompletedStatus.value === FilterStatus.Incomplete) {
+        tasks = tasks.filter((task) => task.completed === 0);
+    } else if (filterCompletedStatus.value === FilterStatus.Complete) {
+        tasks = tasks.filter((task) => task.completed === 1);
+    }
+
+    // ソート処理
+    if (sortStatus.value === SortStatus.DueDate) {
+        tasks = tasks.slice().sort((a, b) => {
+            return (
+                new Date(a.due_date).getTime() - new Date(b.due_date).getTime()
+            );
+        });
+    } else if (sortStatus.value === SortStatus.CreatedAt) {
+        tasks = tasks.slice().sort((a, b) => {
+            return (
+                new Date(a.created_at).getTime() -
+                new Date(b.created_at).getTime()
+            );
+        });
+    }
+
+    return tasks;
+});
 </script>
 <template>
     <h2>一覧ページ</h2>
-    <Link :href="route('tasks.create')">新規追加</Link>
-    <select @change="handleChangeFilter">
-        <option value="0">全て</option>
-        <option value="1">未完了</option>
-        <option value="2">完了済み</option>
+    <Link :href="route('tasks.create')">新規追加</Link><br />
+    <label for="filter">フィルター: </label>
+    <select id="filter" v-model="filterCompletedStatus">
+        <option :value="FilterStatus.All">全て</option>
+        <option :value="FilterStatus.Incomplete">未完了</option>
+        <option :value="FilterStatus.Complete">完了済み</option></select
+    ><br />
+
+    <label for="sort">並び替え: </label>
+    <select id="sort" v-model="sortStatus">
+        <option :value="SortStatus.DueDate">締切日順</option>
+        <option :value="SortStatus.CreatedAt">作成日順</option>
     </select>
-    <TaskListTable :tasks="filterCompletedTasks" />
+    <TaskListTable :tasks="filteredAndSortedTasks" />
 </template>
